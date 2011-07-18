926ead73e05c25a08af9f0f05f2aead7c8f8286f
Joseph Wynn <wildlyinaccurate@gmail.com>
2011-07-18 22:27:55 +0100

    Added leading backslash to namespaces

--- application/controllers/setup.php
+++ application/controllers/setup.php
@@ -7,17 +7,17 @@ class Setup extends WL_Controller {
 
 	public function index()
 	{
-		$group = new models\UserGroup;
+		$group = new \models\UserGroup;
 		$group->setName('User');
 		$group->setLevel(1);
 		$this->em->persist($group);
 
-		$group = new models\UserGroup;
+		$group = new \models\UserGroup;
 		$group->setName('Admin');
 		$group->setLevel(8);
 		$this->em->persist($group);
 
-		$group = new models\UserGroup;
+		$group = new \models\UserGroup;
 		$group->setName('Super Admin');
 		$group->setLevel(9);
 		$this->em->persist($group);
