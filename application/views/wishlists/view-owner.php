daff457c1074e475cf741cc7b453e39d8b166664
Joseph Wynn <wildlyinaccurate@gmail.com>
2011-07-19 22:17:54 +0100

    Changed styling of buttons and inputs so that they line up nicer

--- application/views/wishlists/view-owner.php
+++ application/views/wishlists/view-owner.php
@@ -1,3 +1,9 @@
+<?php if ($wishlist->isPublic()): ?>
+	<p>This wish list can be <?=anchor("wishlists/share/{$wishlist->getId()}", 'viewed by everybody')?>. Don't feel like sharing? <?=anchor('', 'Make this wish list private')?></p>
+<?php else: ?>
+	<p>This wish list is private. Want to share it with friends and family? <?=anchor('', 'Make this wish list public')?></p>
+<?php endif; ?>
+
 <?=$new_item_form?>
 
 <ul id="wishlist-items">
@@ -6,5 +12,4 @@
 	<?php else: ?>
 		<li>There are no items in this wish list.</li>
 	<?php endif; ?>
-</ul>
 </ul>
