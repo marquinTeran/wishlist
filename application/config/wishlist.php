f6b79aaafd21da93e20f74f059473d666c8948c6
Joseph Wynn <wildlyinaccurate@gmail.com>
2011-07-23 15:14:17 +0100

    Tidied the language section

--- application/config/wishlist.php
+++ application/config/wishlist.php
@@ -41,21 +41,19 @@ $config['default_language'] = 'en';
 | Languages
 |--------------------------------------------------------------------------
 |
-| available_languages - An array of languages that are available for translation
 | language_cookie - The name of the cookie in which to store the current language
 | language_cookie_expire - Expiry time of the language cookie. Default: 2 weeks (1209600)
+| available_languages - An array of languages that are available for translation
 |
 */
+$config['language_cookie'] = 'wishlist-language';
+$config['language_cookie_expire'] = 1209600;
 $config['available_languages'] = array(
 	'en' => array(
 		'name' => 'English',
 		'folder' => 'english'
 	)
 );
-
-$config['language_cookie'] = 'wishlist-language';
-$config['language_cookie_expire'] = 1209600;
-
 
 /* End of file wishlist.php */
 /* Location: ./application/config/wishlist.php */
