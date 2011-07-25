bb1f6b0fc39eafeda8de656fb2d2b918894a05c6
Joseph Wynn <wildlyinaccurate@gmail.com>
2011-07-25 22:12:11 +0100

    Removed the __call override method. Pretty useless and promotes bad coding

--- application/models/BaseModel.php
+++ application/models/BaseModel.php
@@ -9,20 +9,20 @@ namespace models;
  *
  * @package		Wishlist
  * @subpackage	Classes
- * @category	Magic
+ * @category	Models
  * @author		Joseph Wynn
  * @since		Version 0.1
  */
-class BaseModel
-{
+class BaseModel {
 
 	/**
 	 * Constructor
-	 * 
+	 *
 	 * @access	public
 	 */
 	public function __construct()
 	{
+		// Nothing here yet!
 	}
 
     /**
@@ -65,69 +65,5 @@ class BaseModel
 
         return $this_as_array;
     }
-
-	/**
-	 * __call override method
-	 *
-	 * Attempt to set/get a property. Only supports all-lowercase, underscore-separated
-     * properties i.e. $this_is_a_property can be accessed using getThisIsAProperty().
-     *
-	 * This method differs from __set() and __get(), as we want to be able to have custom get()
-	 * and set() methods (e.g. setPassword() encrypts the password before setting it).
-	 *
-	 * set() methods will return the Object to allow method chaining.
-	 *
-	 * Note that this method can be very slow and it is recommended that you set up
-	 * get() and set() methods where possible.
-	 *
-	 * @access	public
-     * @throws  Exception
-	 * @param	string	$method
-	 * @param	array	$args
-	 * @return	object|mixed
-     *
-	 */
-	public function __call($method, $args)
-	{
-		// Find words by camelCase (e.g. setUsername, getUserGroup)
-		$method_words = preg_split('/(?=[A-Z])/', $method);
-		$method_name = $method_words[0];
-
-		// Remove the method name from method_words
-		unset($method_words[0]);
-
-		// The remaining method_words make up the property name
-		// UserGroup becomes user_group
-		$property = strtolower(implode('_', $method_words));
-
-		if ($method_name != 'set' && $method_name != 'get')
-		{
-            throw new Exception("Tried to call undefined function {$method}() on class " . get_class($this));
-		}
-
-		// Property doesn't exist
-		if ( ! property_exists($this, $property))
-		{
-            throw new Exception("Tried to call {$method}() on " . get_class($this) . ". Property '{$property}' doesn't exist.");
-		}
-
-		// Set() methods
-		if ($method_name == 'set')
-		{
-			// More than one argument was given
-			if (count($args) > 1)
-			{
-                throw new Exception("Tried to set " . get_class($this) . "->{$property}. 1 argument expected; " . count($args) . " given.");
-			}
-
-			$this->$property = $args[0];
-			return $this;
-		}
-
-		// Get() methods
-		if ($method_name == 'get')
-		{
-			return $this->$property;
-		}
-	}
+	
 }
